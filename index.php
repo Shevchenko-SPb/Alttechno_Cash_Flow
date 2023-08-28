5b1b051b1aa7d9b13f99c143b0ee43fb8804bc37
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-08-28 11:33:10 +0300

    .

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
+<?php
+$connection = \Bitrix\Main\Application::getConnection();
+$sql = $connection->Query("SELECT biep.IBLOCK_ELEMENT_ID FROM b_iblock_element_property biep WHERE `IBLOCK_ELEMENT_ID` IN (SELECT `ID` FROM `b_iblock_element` WHERE `IBLOCK_ID` = 48) 
+AND `IBLOCK_PROPERTY_ID` = 407
+#SELECT `VALUE` FROM `b_iblock_element_property` WHERE `IBLOCK_ELEMENT_ID` IN (SELECT `ID` FROM `b_iblock_element` WHERE `IBLOCK_ID` = 48) 
+#AND `IBLOCK_PROPERTY_ID` = 407
+AND `IBLOCK_ELEMENT_ID` IN (SELECT `IBLOCK_ELEMENT_ID` FROM `b_iblock_element_property` WHERE `IBLOCK_ELEMENT_ID` IN (SELECT `ID` FROM `b_iblock_element` WHERE `IBLOCK_ID` = 48) 
+AND `IBLOCK_PROPERTY_ID` = 405 AND `VALUE` BETWEEN '2023-06-01' AND '2023-08-24');");
 
-<!--$connection = \Bitrix\Main\Application::getConnection();-->
-<!--$sql = $connection->Query("SELECT biep.IBLOCK_ELEMENT_ID FROM b_iblock_element_property biep WHERE `IBLOCK_ELEMENT_ID` IN (SELECT `ID` FROM `b_iblock_element` WHERE `IBLOCK_ID` = 48) -->
-<!--AND `IBLOCK_PROPERTY_ID` = 407-->
-<!--#SELECT `VALUE` FROM `b_iblock_element_property` WHERE `IBLOCK_ELEMENT_ID` IN (SELECT `ID` FROM `b_iblock_element` WHERE `IBLOCK_ID` = 48) -->
-<!--#AND `IBLOCK_PROPERTY_ID` = 407-->
-<!--AND `IBLOCK_ELEMENT_ID` IN (SELECT `IBLOCK_ELEMENT_ID` FROM `b_iblock_element_property` WHERE `IBLOCK_ELEMENT_ID` IN (SELECT `ID` FROM `b_iblock_element` WHERE `IBLOCK_ID` = 48) -->
-<!--AND `IBLOCK_PROPERTY_ID` = 405 AND `VALUE` BETWEEN '2023-06-01' AND '2023-08-24');");-->
-<!---->
-<!---->
-<!--foreach ($sql  as $row)-->
-<!--{-->
-<!--    echo $row['IBLOCK_ELEMENT_ID'];-->
-<!---->
-<!--}-->
-<!--?>-->
+
+foreach ($sql  as $row)
+{
+    echo $row['IBLOCK_ELEMENT_ID'];
+
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
