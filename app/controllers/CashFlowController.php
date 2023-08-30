b96379b5f96272fb24b13ab66540abfca1e18926
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-08-31 00:05:24 +0300

    .

--- app/controllers/CashFlowController.php
+++ app/controllers/CashFlowController.php
@@ -10,9 +10,7 @@ class CashFlowController {
             $result = $v->render('index.html');
             echo $result;
     }
-
     public function actionGetListByDate ($date) {
         // $model = new DealFromCashInModel();
-
     }
 }
